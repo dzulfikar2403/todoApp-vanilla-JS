3f25eb44a5b7d08c1694d283790ce0f70a87e603
dzulfikar2403 <dzulfikar2403@.com>
2023-12-07 06:56:31 +0700

    first commit

---
+++ public/js/app.js
@@ -0,0 +1,80 @@
+window.addEventListener("load", () => {
+  let inputValue = document.getElementById("inputText");
+  let removeAll = document.getElementById("removeAll");
+  let UlMainList = document.getElementById("main-list");
+  let liNew, labelNew, inputNew, divNew, spanEdit, spanRemove;
+
+  window.addEventListener("keyup", (e) => {
+    if (e.key === "/" && e.ctrlKey) {
+      e.preventDefault(); // untuk mematikan event bawaan dari browser
+      inputValue.focus();
+    }
+
+    if (e.key === "Enter") {
+      mainFunc();
+    }
+  });
+
+  window.addEventListener("click", (e, i) => {
+    if (e.target.id === "singleRemoveClick") e.target.parentElement.parentElement.parentElement.remove();
+    if (e.target.id === "editClick") {
+      if(e.target.textContent === '✏️'){
+        // e.target.parentElement.parentElement.previousElementSibling = inputNew
+        e.target.parentElement.parentElement.previousElementSibling.removeAttribute("readonly");
+        e.target.parentElement.parentElement.previousElementSibling.focus();
+        e.target.textContent = '✔️';
+      }else{
+        e.target.parentElement.parentElement.previousElementSibling.setAttribute("readonly","readonly");
+        e.target.parentElement.parentElement.previousElementSibling.blur();
+        e.target.textContent = '✏️';
+      }
+    }
+
+    if (e.target.id === "addValue") mainFunc();
+
+    if (e.target.id === "text") {
+      e.target.classList.toggle("checked");
+      e.target.previousElementSibling.classList.toggle("activated")
+    }
+  });
+
+  let mainFunc = () => {
+    if(!inputValue.value){
+      let alertShow = document.getElementById('Alert');
+      let confirmAlert = document.getElementById('confirmAlert');
+      alertShow.classList.add('alertShow')
+      confirmAlert.addEventListener('click',()=>{
+        alertShow.classList.remove('alertShow')
+      })
+    }else{
+      liNew = document.createElement("li");
+      labelNew = document.createElement("label");
+      inputNew = document.createElement("input");
+      divNew = document.createElement("div");
+      spanEdit = document.createElement("span");
+      spanRemove = document.createElement("span");
+  
+  
+      inputNew.value = inputValue.value;
+      spanEdit.innerHTML = '<p id="editClick">✏️</p>';
+      spanRemove.innerHTML = '<i id="singleRemoveClick" class="fa-solid fa-trash" style="color: #ed7707;"></i>';
+      spanRemove.setAttribute("id", "singleRemove");
+      spanEdit.setAttribute("id", "edit");
+      inputNew.setAttribute('id','text');
+      inputNew.setAttribute('readonly','readonly');
+      liNew.appendChild(labelNew);
+      liNew.appendChild(inputNew);
+  
+      divNew.appendChild(spanEdit);
+      divNew.appendChild(spanRemove);
+      liNew.appendChild(divNew);
+      
+      UlMainList.append(liNew);
+      inputValue.value = "";
+    }
+  };
+
+  removeAll.addEventListener("click", () => {
+    UlMainList.innerHTML = "";
+  });
+});
