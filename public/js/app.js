bd7e17b6c0875f0e579de852f1d493a9c7a3cf72
dzulfikar2403 <dzulfikar2403@.com>
2023-12-08 20:40:09 +0700

    ok

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,10 @@
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("load", () => {
   let inputValue = document.getElementById("inputText");
   let removeAll = document.getElementById("removeAll");
   let UlMainList = document.getElementById("main-list");
-  let liNew, pLogo, spanText, divNew, spanEdit, spanRemove;
+  let liNew, labelNew, inputNew, divNew, spanEdit, spanRemove;
 
-  //set window event keyup
   window.addEventListener("keyup", (e) => {
-    //set shorthand
     if (e.key === "/" && e.ctrlKey) {
       e.preventDefault(); // untuk mematikan event bawaan dari browser
       inputValue.focus();
@@ -17,96 +15,56 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-
-  //set window event click
   window.addEventListener("click", (e, i) => {
-    //remove per 1 item 
-    if (e.target.id === "singleRemoveClick") {
-      e.target.parentElement.parentElement.parentElement.remove();// remove w event penulusran
-      setData(); //set localStorage
-    }
-
-    //edit per 1 item
+    if (e.target.id === "singleRemoveClick") e.target.parentElement.parentElement.parentElement.remove();
     if (e.target.id === "editClick") {
-      let newVal = e.target.parentElement.parentElement.previousElementSibling;
-      let ChangeValue = document.getElementById("ChangeValue");
-      let cancelChange = document.getElementById("cancelChange");
-      let confirmChange = document.getElementById("confirmChange");
-      let ChangeSpanText = document.getElementById("ChangeSpanText");
-
-      //changeEdit active
-      ChangeSpanText.classList.add('editShow');
-      ChangeValue.focus();
-
-      // cancel data if click cancel
-      cancelChange.addEventListener('click',()=>{
-        ChangeSpanText.classList.remove('editShow');
-        ChangeValue.blur();
-        setData(); //set localStorage
-      });
-      
-      // confirm data if click cancel
-      confirmChange.addEventListener('click',()=>{
-        ChangeSpanText.classList.remove('editShow');
-        ChangeValue.blur();
-        
-        //ambil nilai dulu, baru hapus isi input dari changeValue
-        newVal.textContent = ChangeValue.value;
-        ChangeValue.value = '';
-
-        setData(); //set localStorage
-      });
+      if(e.target.textContent === '✏️'){
+        // e.target.parentElement.parentElement.previousElementSibling = inputNew
+        e.target.parentElement.parentElement.previousElementSibling.removeAttribute("readonly");
+        e.target.parentElement.parentElement.previousElementSibling.focus();
+        e.target.textContent = '✔️';
+      }else{
+        e.target.parentElement.parentElement.previousElementSibling.setAttribute("readonly","readonly");
+        e.target.parentElement.parentElement.previousElementSibling.blur();
+        e.target.textContent = '✏️';
+      }
+      setData();
     }
 
-    //if btn Enter diclick. jlnkn func
     if (e.target.id === "addValue") mainFunc();
 
-    // set active jika task sudah selesai (tag = LI)
-    if (e.target.tagName === "LI") {
-      // take p dari LI & set success
-      e.target.children[0].classList.toggle("activated");
-      e.target.children[1].classList.toggle("checked");
-      
-      setData(); //set localStorage
-    }
-    
-    
-    // set active jika task sudah selesai (id = text)
     if (e.target.id === "text") {
-      // take p dari id text & set success
-      e.target.previousElementSibling.classList.toggle("activated");
       e.target.classList.toggle("checked");
-    
-      setData(); //set localStorage
+      e.target.previousElementSibling.classList.toggle("activated");
+      setData();
     }
   });
 
-  //bikin func utama
   let mainFunc = () => {
-    if (!inputValue.value) {
-      let alertShow = document.getElementById("Alert");
-      let confirmAlert = document.getElementById("confirmAlert");
-      alertShow.classList.add("alertShow");
-      confirmAlert.addEventListener("click", () => {
-        alertShow.classList.remove("alertShow");
-      });
-    } else {
+    if(!inputValue.value){
+      let alertShow = document.getElementById('Alert');
+      let confirmAlert = document.getElementById('confirmAlert');
+      alertShow.classList.add('alertShow')
+      confirmAlert.addEventListener('click',()=>{
+        alertShow.classList.remove('alertShow')
+      })
+    }else{
       liNew = document.createElement("li");
-      pLogo = document.createElement("p");
-      spanText = document.createElement("span");
+      labelNew = document.createElement("label");
+      inputNew = document.createElement("input");
       divNew = document.createElement("div");
       spanEdit = document.createElement("span");
       spanRemove = document.createElement("span");
 
-      spanText.textContent = inputValue.value;
-      // pLogo.setAttribute('for','text');
-      spanEdit.innerHTML = '<span id="editClick">✏️</span>';
+      inputNew.value = inputValue.value;
+      spanEdit.innerHTML = '<p id="editClick">✏️</p>';
       spanRemove.innerHTML = '<i id="singleRemoveClick" class="fa-solid fa-trash" style="color: #ed7707;"></i>';
       spanRemove.setAttribute("id", "singleRemove");
       spanEdit.setAttribute("id", "edit");
-      spanText.setAttribute("id", "text");
-      liNew.appendChild(pLogo);
-      liNew.appendChild(spanText);
+      inputNew.setAttribute('id','text');
+      inputNew.setAttribute('readonly','readonly');
+      liNew.appendChild(labelNew);
+      liNew.appendChild(inputNew);
 
       divNew.appendChild(spanEdit);
       divNew.appendChild(spanRemove);
@@ -118,7 +76,6 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  // hapus semua isi UL dengan keyUl dari localstorage
   removeAll.addEventListener("click", () => {
     UlMainList.innerHTML = "";
     localStorage.removeItem('keyUl');
@@ -126,11 +83,14 @@ window.addEventListener("DOMContentLoaded", () => {
 
   //buat cekData ada/tidak
   let getData = localStorage.getItem("keyUl");
-  // buat localstorage setData for parent LI
-  let setData = () => {
-    localStorage.setItem("keyUl", UlMainList.innerHTML);
-  };
+  if(!getData){
+    console.log('Data tidak ada!')
+  }{
+    UlMainList.innerHTML = getData;
+  }
 
-  // set UL jika sudah memiliki data dari localStorage 
-  UlMainList.innerHTML = getData;
+    // buat localstorage setData for parent LI
+    let setData = () => {
+      localStorage.setItem("keyUl", UlMainList.innerHTML);
+    };
 });
